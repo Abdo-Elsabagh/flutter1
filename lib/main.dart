8e65b5b71d45ec4320d9b967f7f007f9218277dd
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2023-10-11 07:17:55 +0300

    edd comment

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-     home: FirstScreen(),
-     // home: SecondScreen(), //task 1
+     //home: FirstScreen(),
+     home: SecondScreen(), //task 1
       //home: ThreeScreen(),//task 2
     );
   }
